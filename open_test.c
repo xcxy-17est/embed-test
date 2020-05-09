683ea10fdb83631872f4d98b304f656384528b23
gaspar <69740344@qq.com>
2020-05-09 16:18:48 +0800

    add open_test.c

---
+++ open_test.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char sz_filename[] = "README1.md";
+    int fd = -1;
+    fd = open(sz_filename, O_WRONLY);
+    if (fd < 0)
+    {
+        //printf("error\n");
+        perror("open failed");
+        return -1;
+    }
+    printf("open success\n");
+    return 0;
+}
